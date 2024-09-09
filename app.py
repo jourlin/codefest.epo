4d197cf6fa9816f9d9878000651410ebf549af98
Pierre Jourlin <Pierre.Jourlin@univ-avignon.fr>
2024-09-09 08:12:32 +0200

    +flask commands

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template
+import click
+from toolkit import Toolkit
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.cli.command("reindex")
+@click.argument("index_name")
+def reindex(index_name):
+    """Regenerate the Deeplake store."""
+    t=Toolkit(index_name)
+    t.reindex()
+
+    
---
+++ toolkit.py
@@ -0,0 +1,7 @@
+class Toolkit:
+    def __init__(self, name):
+        self.name=name
+
+    def reindex(self):
+        print(f"Reindexing '{self.name}'...")
+        print("Completed.")
